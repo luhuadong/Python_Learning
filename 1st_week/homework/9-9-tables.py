139107c14daf5dc0b390fa06ed39252f1771f9c9
luhuadong <luhuadong@163.com>
2018-06-18 15:59:04 +0800

    add comment

--- 1st_week/homework/9-9-tables.py
+++ 1st_week/homework/9-9-tables.py
@@ -1,12 +1,16 @@
-# 九九乘法表
+'''
+ Name: 九九乘法表
+ Author: 卢华东
+ Date: 2018-06-18
+'''
 
-'''
- For in 方法
-'''
+# For in 方法
 
 print("")
 print("#"*72)
 print("\t\t\t\t[for in]")
+
+# 横向: 1->9  纵向: 1->9  左对齐
 
 for i in range(1, 10):
     for j in range(1, i+1):
@@ -14,6 +18,8 @@ for i in range(1, 10):
     print("")
 
 print("-"*72)
+
+# 横向: 1->9  纵向: 9->1  左对齐
 
 for i in range(9, 0, -1):
     for j in range(1, i+1):
@@ -22,6 +28,8 @@ for i in range(9, 0, -1):
 
 print("="*72)
 
+# 横向: 9->1  纵向: 1->9  右对齐
+
 for i in range(1, 10):
     print(" "*8*(9-i), end="")
     for j in range(i, 0, -1):
@@ -30,18 +38,21 @@ for i in range(1, 10):
 
 print("-"*72)
 
+# 横向: 9->1  纵向: 9->1  右对齐
+
 for i in range(9, 0, -1):
     print(" "*8*(9-i), end="")
     for j in range(i, 0, -1):
         print("{}*{}={:<3}".format(j,i,i*j), end=" ")
     print("")
 
-'''
- While 方法
-'''
+# While 方法
+
 print("")
 print("#"*72)
 print("\t\t\t\t[while]")
+
+# 横向: 1->9  纵向: 1->9  左对齐
 
 i=1
 while i<10:
@@ -53,6 +64,8 @@ while i<10:
     print("")
 
 print("-"*72)
+
+# 横向: 1->9  纵向: 9->1  左对齐
 
 i=9
 while i>0:
@@ -65,6 +78,8 @@ while i>0:
 
 print("="*72)
 
+# 横向: 9->1  纵向: 1->9  右对齐
+
 i=1
 while i<10:
     j=i
@@ -76,6 +91,8 @@ while i<10:
     print("")
 
 print("-"*72)
+
+# 横向: 9->1  纵向: 9->1  右对齐
 
 i=9
 while i>0:
