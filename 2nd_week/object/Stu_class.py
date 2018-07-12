5ffc1cd8f9e00aa33a9e9f2809668bce490d960d
luhuadong <luhuadong@163.com>
2018-07-12 17:45:05 +0800

    add practise of class & object

---
+++ 2nd_week/object/Stu_class.py
@@ -0,0 +1,32 @@
+
+class People:
+
+    name = ""
+
+    def __init__(self, name):
+        self.name = name
+
+    def getName(self):
+        return self.name
+
+class Student(People):
+    grade = ""
+
+    def __init__(self, name, grade):
+        People.__init__(self, name)
+        self.grade = grade
+
+    def getName(self):
+        return ("Name:" + self.name)
+
+    def getGrade(self):
+        return self.grade
+
+person = People("Rudy")
+print(person.getName())
+#print(person.name)
+
+stu = Student("Tina", "A+")
+print(stu.getName())
+print(stu.getGrade())
+
