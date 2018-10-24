531b90329cafa52ba54e37e6d71b01a9d55d8337
luhuadong <luhuadong@163.com>
2018-10-24 17:26:06 +0800

    update run...

--- 13th_week/homework/linuxmm/static/run.py
+++ 13th_week/homework/linuxmm/static/run.py
@@ -2,11 +2,14 @@ python3 <<EOF
 import psutil as pu
 import json
 info = {}
+info['cpu_count'] = pu.cpu_count()
+info['cpu_percent'] = pu.cpu_percent()
 mem = pu.virtual_memory()
 info['mem_total'] = mem.total
 info['mem_available'] = mem.available
 info['mem_percent'] = mem.percent
 info['mem_used'] = mem.used
 info['mem_free'] = mem.free
-print(info)
+data = json.dumps(info)
+print(data)
 EOF
