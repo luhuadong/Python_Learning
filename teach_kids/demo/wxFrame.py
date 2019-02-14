f1a55859fb2c555dabd2f27e81df3aa198864ed8
luhuadong <luhuadong@163.com>
2019-02-14 22:37:13 +0800

    地图数据

--- teach_kids/demo/wxFrame.py
+++ teach_kids/demo/wxFrame.py
@@ -1,7 +1,16 @@
 import wx
 
 app = wx.App()
-win = wx.Frame(None, title)
+win = wx.Frame(None, title="翻牌子", size=(400, 300))
+
+numText = wx.StaticText(win, label="Who?", size=(400, 150), pos=(0, 50), style=wx.ALIGN_CENTER)
+
+font = numText.GetFont()
+font.PointSize += 60
+font = font.Bold()
+numText.SetFont(font)
+
+startBtn = wx.Button(win, label="点名", size=(100, 50), pos=(150, 200))
 
 
 win.Show()
---
+++ teach_kids/json-data/word_population.py
@@ -0,0 +1,11 @@
+import json
+
+filename = 'population_data.json'
+with open(filename) as f:
+    pop_data = json.load(f)
+
+for pop_dict in pop_data:
+    if pop_dict['Year'] == '2014':
+        country_name = pop_dict['Country Name']
+        population = int(pop_dict['Value'])
+        print(country_name + ": " + str(population))
